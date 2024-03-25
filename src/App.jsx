b6500ab86a99b51c052ebcc1bbdc24fce37e7634
Noman Ali <noman.sd114@gmail.com>
2024-03-25 20:06:08 +0500

    project created and integerated tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-7xl bg-red-500'>heloo</h1>
+    </>
+  )
+}
+
+export default App
