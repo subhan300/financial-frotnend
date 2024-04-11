7df65d020f464f7934e821b6094dc0dc1e0b5006
subhan300 <subhan.akram2400@gmail.com>
2024-04-12 02:43:46 +0500

    work updated

--- src/utils/makeRequest.js
+++ src/utils/makeRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getUserToken } from './Utils';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000', // Base API URL
+  baseURL: 'https://financial-management-backend-one.vercel.app/', // Base API URL
   headers: {
     'Content-Type': 'application/json',
   },
