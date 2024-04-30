3f9987c171edb1385a305fd9cf189aac50d07009
subhan300 <subhan.akram2400@gmail.com>
2024-05-01 03:51:43 +0500

    base url added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://financial-frotnend.vercel.app/',
   define: {
     'process.env': process.env
   },
